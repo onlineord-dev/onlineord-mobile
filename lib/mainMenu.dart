ca9fe7a184139f479fa43fc6c7757ee85a2fd48a
vladamizerak <mizerak.vlada@gmail.com>
2021-01-21 16:25:36 +0200

    Add pages

---
+++ lib/mainMenu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainMenu());
+}
+
+class MainMenu extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Menu'
+    );
+  }
+}
