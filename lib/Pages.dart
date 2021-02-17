6656a18554c6e69b1365c0a0e536306339a14521
Ilya <crosbu2002@gmail.com>
2021-02-17 21:54:09 +0200

    QRCode class changed

--- lib/Pages.dart
+++ lib/Pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class LogInPage extends StatelessWidget {
   @override
@@ -67,18 +68,66 @@ class LogInPage extends StatelessWidget {
   }
 }
 
-class QRCodePage extends StatelessWidget {
+class QRCodePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  GlobalKey qrKey = GlobalKey();
+  Barcode qrText;
+  QRViewController controller;
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        appBar: AppBar(title: Text("QR Scan")),
-        body: Center(
-          child: RaisedButton(
+      appBar: AppBar(title: Text("QR Scan")),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+                key: qrKey,
+                overlay: QrScannerOverlayShape(
+                    borderRadius: 10,
+                    borderColor: Colors.red,
+                    borderLength: 30,
+                    borderWidth: 10,
+                    cutOutSize: 300),
+                onQRViewCreated: _onQRViewCreated),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Scan result: $qrText'),
+            ),
+          ),
+          Center(
+            child: RaisedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/Main');
               },
-              child: Text("Scan")),
-        ));
+            ),
+
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
   }
 }
 
