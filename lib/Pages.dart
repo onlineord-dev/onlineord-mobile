6650766a11bc71be88ee0354b11466e7492947ba
Ilya <crosbu2002@gmail.com>
2021-03-26 17:52:52 +0200

    QRCode page added, class QrCode deleted from Pages.

--- lib/Pages.dart
+++ lib/Pages.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'QRScan_Page.dart';
-
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class LogInPage extends StatelessWidget {
   @override
@@ -11,7 +8,7 @@ class LogInPage extends StatelessWidget {
       appBar: AppBar(title: Text("Log In")),
       body: Center(
         child:Padding(
-          padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -57,183 +54,168 @@ class LogInPage extends StatelessWidget {
                         primary: Colors.blue,
                         onPrimary: Colors.black
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/Main');
-                    },
-                    child: Text("Вхід")
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Main');
+                  },
+                  child: Text("Вхід")
                 ),
               ),
             ],
           ),
-        ),
+          ),
       ),
     );
   }
 }
+
 
 class RegistrPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Registration")),
-      body: Center(
+        appBar: AppBar(title: Text("Registration")),
+        body: Center(
         child:Padding(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Пошта',
-                    ),
-                    keyboardType: TextInputType.emailAddress ,
-                    textInputAction: TextInputAction.next,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        /*code*/
-                      }
-                      return null;
-                    }
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Ім’я',
-                  ),
-                  keyboardType: TextInputType.text ,
-                  textInputAction: TextInputAction.next,
-                  validator:(value) {
-                    if (value.isEmpty) {
-                      /*code*/
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Прізвище',
-                  ),
-                  textInputAction: TextInputAction.next,
-                  validator:(value) {
-                    if (value.isEmpty) {
-                      /*code*/
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Номер телефону',
-                    prefixText: '+38',
-                  ),
-                  keyboardType: TextInputType.phone ,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+              TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Пошта',
+              ),
+                  keyboardType: TextInputType.emailAddress ,
                   textInputAction: TextInputAction.next,
                   validator: (value) {
                     if (value.isEmpty) {
                       /*code*/
                     }
                     return null;
-                  },
+                  }
+              ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Ім’я',
+              ),
+              keyboardType: TextInputType.text ,
+              textInputAction: TextInputAction.next,
+              validator:(value) {
+                if (value.isEmpty) {
+                  /*code*/
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Прізвище',
+              ),
+              textInputAction: TextInputAction.next,
+              validator:(value) {
+                if (value.isEmpty) {
+                  /*code*/
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Номер телефону',
+                prefixText: '+38',
+              ),
+              keyboardType: TextInputType.phone ,
+              textInputAction: TextInputAction.next,
+              validator: (value) {
+                if (value.isEmpty) {
+                  /*code*/
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Пароль',
+              ),
+              keyboardType: TextInputType.text ,
+              textInputAction: TextInputAction.next,
+              obscureText: true,
+              validator: (value) {
+                if (value.isEmpty) {
+                  /*code*/
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Підтвердіть пароль',
+              ),
+              keyboardType: TextInputType.text ,
+              obscureText: true,
+              validator: (value) {
+                if (value.isEmpty) {
+                  /*code*/
+                }
+                return null;
+              },
+            ),
+            Center(
+              child:FlatButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/LogIn');
+                },
+                child: Text(
+                  "Вже зареєстровані? Увійти.",
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Пароль',
+                textColor: Colors.lightBlue,
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.black
                   ),
-                  keyboardType: TextInputType.text ,
-                  textInputAction: TextInputAction.next,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      /*code*/
-                    }
-                    return null;
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Main');
                   },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Підтвердіть пароль',
-                  ),
-                  keyboardType: TextInputType.text ,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      /*code*/
-                    }
-                    return null;
-                  },
-                ),
-                Center(
-                  child:FlatButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/LogIn');
-                    },
-                    child: Text(
-                      "Вже зареєстровані? Увійти.",
-                    ),
-                    textColor: Colors.lightBlue,
-                  ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
-                          onPrimary: Colors.black
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/Main');
-                      },
-                      child: Text("Створити аккаунт")),
-                ),
-              ]
+                  child: Text("Створити аккаунт")),
+              ),
+            ]
           ),
         ),
       ),
     );
   }
 }
-
 
 class MyApp extends StatefulWidget {
   @override
   MainPage createState() => MainPage();
 }
 
-
 class MainPage extends State<MyApp> {
   int _currentIndex = 0;
-  var _pages = [
-    QRScan(),
-    Text('Домашня'),
-    Text('Меню'),
-    Text('Корзина')
-  ];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(title: Text("Main")),
-      body: SafeArea(
-        top: false,
-        child: _pages.elementAt(_currentIndex),
-      ),
+      body: Container(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.qr_code),
-              label: 'Scan',
-              backgroundColor: Colors.blue),
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: 'Home',
               backgroundColor: Colors.blue),
           BottomNavigationBarItem(
-              icon: Icon(Icons.restaurant_menu),
-              label: 'Menu',
-              backgroundColor: Colors.blue),
+              icon: Icon(Icons.qr_code),label: 'Scan', backgroundColor: Colors.blue),
           BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_basket),
-              label: 'Basket',
-              backgroundColor: Colors.blue),
+              icon: Icon(Icons.restaurant_menu),label: 'Menu', backgroundColor: Colors.blue),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_basket), label: 'Basket',backgroundColor: Colors.blue),
         ],
         onTap: (index) {
           setState(() {
@@ -250,6 +232,7 @@ void main() {
     initialRoute: '/LogIn',
     routes: {
       '/LogIn': (BuildContext context) => LogInPage(),
+      //'/QRScan': (BuildContext context) => QRCodePage(), //Навігація на іншу сторінку.
       '/LogIn/Registr': (BuildContext context) => RegistrPage(),
       '/Main': (BuildContext context) => MyApp(),
     },
--- lib/QRScan_Page.dart
+++ lib/QRScan_Page.dart
@@ -1,7 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QRScan extends StatelessWidget{
+class QRScan extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRScan> {
+  GlobalKey qrKey = GlobalKey();
+  Barcode qrText;
+  QRViewController controller;
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("QR Scan")),
+
+      body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/Main');
+            },
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 5,
+                  child: QRView(
+                      key: qrKey,
+                      overlay: QrScannerOverlayShape(
+                          borderRadius: 10,
+                          borderColor: Colors.red,
+                          borderLength: 30,
+                          borderWidth: 10,
+                          cutOutSize: 300),
+                      onQRViewCreated: _onQRViewCreated),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Center(
+                    child: Text('Scan result: $qrText'),
+                  ),)
+              ],
+            ),
+
+          )
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
   }
 }
