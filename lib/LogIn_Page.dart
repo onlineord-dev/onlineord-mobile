e5c28d4191446efa072798ec24740cf49e14790f
Ilya <crosbu2002@gmail.com>
2021-04-04 00:56:59 +0300

    LogIn тепер увівши правельну пошту та пароль можна увійьт у додаток.

--- lib/LogIn_Page.dart
+++ lib/LogIn_Page.dart
@@ -10,8 +10,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
 
 
-TextEditingController EmailController = new TextEditingController();
-TextEditingController PasswordController = new TextEditingController();
+
 
 
 
@@ -24,26 +23,43 @@ class LogInPage extends StatefulWidget {
 }
 
 class _LogInPageState extends State<LogInPage> {
+  TextEditingController emailController =  TextEditingController();
+  TextEditingController passwordController =  TextEditingController();
 
-  bool _isLoading = false;
-  signIn(String Email,String Password) async {
+
+  bool _isLoading = true;
+  signIn(String email,String password) async {
+    String url = "https://onlineord.herokuapp.com/users/";
     Map data = {
-      'Email': Email,
-      'Password': Password
+      'email': email,
+      'password': password
     };
-    var jsonData = null;
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    var response = await http.post('https://cdn.jsdelivr.net/gh/onlineord-dev/onlineord-server/mock/users.json', body: data);
+    var jsonData ;
+
+
+    var response = await http.post(url, body: data);
+
     if(response.statusCode == 200){
       jsonData = json.decode(response.body);
+      print("Response status: ${response.statusCode}");
+
+      print("Response status: ${response.body}");
+
+      if(jsonData != null && jsonData[0].keys.toString() != "(error)"){
+        setState((){
+          _isLoading = true;
+        });
+
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MyApp()), (Route<dynamic> route) => false);
+      }
+    } else {
       setState((){
         _isLoading = false;
-        sharedPreferences.setString("token", jsonData['token']);
-        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MyApp()), (Route<dynamic> route) => false);
+
       });
-    }
-    else {
-      print(response.body);
+
+      print("Response status: ${response.body}");
+
     }
   }
 
@@ -54,35 +70,35 @@ class _LogInPageState extends State<LogInPage> {
       body: Center(
         child:Padding(
           padding: EdgeInsets.all(8.0),
-          child: _isLoading ? Center(child: CircularProgressIndicator()) : Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               TextFormField(
-                controller: EmailController,
+                controller: emailController,
                 decoration: const InputDecoration(
-                  labelText: 'Логін або пошта',
+                  hintText: 'Логін або пошта',
                 ),
                 textInputAction: TextInputAction.next,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    /*code*/
-                  }
-                  return null;
-                },
+                // validator: (value) {
+                //   if () {
+                //     //
+                //   }
+                //   return null;
+                // },
               ),
               TextFormField(
-                controller: PasswordController,
+                controller: passwordController,
                 decoration: const InputDecoration(
-                  labelText: 'Пароль',
+                  hintText: 'Пароль',
                 ),
                 obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    /*code*/
-                  }
-                  return null;
-                },
+                // validator: (value) {
+                //   if () {
+                //
+                //   }
+                //   return null;
+                // },
               ),
               Center(
                 child:FlatButton(
@@ -99,17 +115,13 @@ class _LogInPageState extends State<LogInPage> {
                 child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         primary: Colors.blue,
-                        onPrimary: Colors.black
-                    ),
-                    onPressed: () {
+                        onPrimary: Colors.black),
+
+                      onPressed : () {
                       // Navigator.pushNamed(context, '/Main');
-                      setState(() {
-                        _isLoading = true;
-                      });
-                      signIn(EmailController.text, PasswordController.text);
-
-                    },
-
+                      setState(() { _isLoading = true;});
+                      signIn(emailController.text, passwordController.text);
+                      },
                     child: Text("Вхід")
                 ),
               ),
