891fd798b3662860ca42d7af8a31c8fe8d469fa5
Ilya <crosbu2002@gmail.com>
2021-05-17 19:09:30 +0300

    QRCode page added, class QrCode deleted from Pages.

--- lib/Menu_Page.dart
+++ lib/Menu_Page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'JsonParseMenu.dart';
 
 class Menu extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: JsonParseMenu(),
+    );
   }
 }
